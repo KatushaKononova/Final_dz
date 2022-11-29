c545275f789465ff4ae7be06c43d1220b46955eb
KatushaKononova56 <83833827+KatushaKononova56@users.noreply.github.com>
2022-11-29 20:36:22 +0300

    добавлена задача

--- task/Program.cs
+++ task/Program.cs
@@ -6,7 +6,33 @@ namespace task
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            Console.WriteLine("Введите слова (через пробел)");
+            string str=Console.ReadLine();
+            string[] str_arr=str.Split(' ');
+            int size = str_arr.Length;
+            FindWords(str_arr, size);
+        }
+
+        static void FindWords(string[] string_words, int length){
+            int kolvo = 0;
+            for(int i=0;i<length;i++){
+                if(string_words[i].Length<=3){
+                    kolvo++;
+                }
+            } 
+            if(kolvo==0){
+                Console.WriteLine("Таких слов нет");
+                return;
+            }
+            else{
+                for(int i=0;i<length;i++){
+                    if(string_words[i].Length<=3){
+                        Console.WriteLine(string_words[i]);
+                    }
+                } 
+            }
+            
         }
     }
 }
